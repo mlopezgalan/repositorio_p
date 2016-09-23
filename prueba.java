fdef5b0be0aacd9060eeb9c64fe5b19050e36746
Miguel López <lopezgalan.miguel@gmail.com>
2016-09-23 16:13:20 +0200

    mi primer comando hola mundo

---
+++ prueba.java
@@ -0,0 +1,17 @@
+package prueba;
+
+/**
+ *
+ * @author Miguel
+ */
+public class Prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Hola mundo");
+    }
+    
+}
